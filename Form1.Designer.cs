39d61c1fa3d12ee9b8f16026ebd59a93c5d10771
수환 김 <leas019@suwon.ac.kr>
2026-04-02 17:24:26 +0900

    과제 2 코딩과 테스트 완료 -로그인 실패 시 출력되는 메세지박스를 에러메세지박스로 변경 -로그인 실패 시 작은 글씨로 실패문구가 나오게 출력 평상시에는 보이지 않음

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@
             txtID = new TextBox();
             txtPW = new TextBox();
             btnLogin = new Button();
+            lblErrorMsg = new Label();
             SuspendLayout();
             // 
             // lblAppName
@@ -55,6 +56,7 @@
             txtID.TabIndex = 2;
             txtID.Text = "아이디";
             txtID.Enter += txtID_Enter;
+            txtID.KeyDown += txtID_KeyDown;
             txtID.Leave += txtID_Leave;
             // 
             // txtPW
@@ -67,6 +69,7 @@
             txtPW.TabIndex = 3;
             txtPW.Text = "패스워드";
             txtPW.Enter += txtPW_Enter;
+            txtPW.KeyDown += txtPW_KeyDown;
             txtPW.Leave += txtPW_Leave;
             // 
             // btnLogin
@@ -74,7 +77,7 @@
             btnLogin.BackColor = Color.FromArgb(192, 192, 255);
             btnLogin.Font = new Font("맑은 고딕", 25F);
             btnLogin.ForeColor = Color.Blue;
-            btnLogin.Location = new Point(260, 315);
+            btnLogin.Location = new Point(260, 326);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(277, 99);
             btnLogin.TabIndex = 1;
@@ -82,11 +85,23 @@
             btnLogin.UseVisualStyleBackColor = false;
             btnLogin.Click += btnLogin_Click;
             // 
+            // lblErrorMsg
+            // 
+            lblErrorMsg.AutoSize = true;
+            lblErrorMsg.ForeColor = Color.IndianRed;
+            lblErrorMsg.Location = new Point(111, 292);
+            lblErrorMsg.Name = "lblErrorMsg";
+            lblErrorMsg.Size = new Size(282, 20);
+            lblErrorMsg.TabIndex = 4;
+            lblErrorMsg.Text = "아이디 또는 비밀번호가 잘못되었습니다.";
+            lblErrorMsg.Visible = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblErrorMsg);
             Controls.Add(btnLogin);
             Controls.Add(txtPW);
             Controls.Add(txtID);
@@ -103,5 +118,6 @@
         private TextBox txtID;
         private TextBox txtPW;
         private Button btnLogin;
+        private Label lblErrorMsg;
     }
 }
